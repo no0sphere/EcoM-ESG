a50b7d94ae3991fb6ce1c4291d3d5e417de14017
Hao Zeng <z5445039@ad.unsw.edu.au>
2024-06-21 16:52:30 +1000

    npm install router

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { BrowserRouter as Router } from 'react-router-dom';
+
+//import PageList from './PageList';
+
+
+const Main = () => {
+    return (
+        <>
+            <Router>
+                <App />
+            </Router>
+        </>
+    );
+}
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+    <React.StrictMode>
+        <Main />
+    </React.StrictMode>
+);
+
+export default Main;
