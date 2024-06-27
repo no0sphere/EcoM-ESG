b0f63840e796c71e0b671b6246e66154d3aea5ef
Xinjun <xinjun.tan@student.unsw.edu.au>
2024-06-27 15:45:00 +1000

    modified:   src/components/Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,20 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <header className="d-flex justify-content-between align-items-center p-2 border-bottom">
-            <div className="h3 mb-0">Mekboy ESG Platform</div>
+            <Link to="/SingleMode" className="text-dark" style={{ textDecoration: 'none' }}> 
+                <div className="h3 mb-0">Mekboy ESG Platform</div>
+            </Link>
             <button className="btn btn-link">
                 <Link to="/help">Help</Link> 
             </button>
             <div>
                 <button className="btn btn-primary mr-3">
                     <Link className="text-white" to="/download-report" style={{ textDecoration: 'none' }}>Download Report</Link>
+                </button>
+            </div>
+            <div>
+                <button className="btn btn-primary mr-3">
+                    <Link className="text-white" to="/setting" style={{ textDecoration: 'none' }}>Setting</Link>
                 </button>
             </div>
             <div>
