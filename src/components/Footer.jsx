7c43cc75ce89ee271aaeb211e2d0d2b762d9054b
Xinjun <xinjun.tan@student.unsw.edu.au>
2024-07-10 23:34:08 +1000

    modified:   src/components/Footer.jsx 	modified:   src/components/Header.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -34,8 +34,10 @@ const Footer = () => {
                             <li><Link to="/about" className="link-dark">About Us</Link></li>
 
                             <li><Link to="/SingleMode" className="link-dark">Dashboard</Link></li>
-
+                            <li><Link to="/frameSelect" className="link-dark">frameSelect</Link></li>{/*should be moved to subheader*/}
                             <li><Link to="/help" className="link-dark">Helping Document</Link></li>
+                            
+                      
                         </ul>
                     </div>
                 </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Header() {
     return (
-        <header className="d-flex justify-content-between align-items-center p-2 border-bottom">
+        <header className="d-flex justify-content-between align-items-center p-1 border-bottom">
             <Link to="/SingleMode" className="text-dark" style={{ textDecoration: 'none' }}> 
                 <div className="h3 mb-0">EcoM ESG Platform</div>
             </Link>
@@ -15,6 +15,8 @@ function Header() {
                     <Link className="text-white" to="/download-report" style={{ textDecoration: 'none' }}>Download Report</Link>
                 </button>
             </div>
+
+
             <div>
                 <button className="btn btn-primary mr-3">
                     <Link className="text-white" to="/setting" style={{ textDecoration: 'none' }}>Setting</Link>
