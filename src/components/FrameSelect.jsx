9bfdcb253796ec5ba7403f40589cd1947f11e29b
SinceDreamTen <jim-tan@foxmail.com>
2024-07-15 01:34:31 +1000

    modified:   src/components/FrameSelect.jsx

--- src/components/FrameSelect.jsx
+++ src/components/FrameSelect.jsx
@@ -1,8 +1,125 @@
-import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Link} from "react-router-dom";
-
+import MockAdapter from 'axios-mock-adapter';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 const FrameSelect = () => {
+    const mock = new MockAdapter(axios);
+    mock.onGet('/api/current_framework?user_name="qq"').reply(200, {
+        "code" : 200,
+        "status": "succeed",
+        "message": "Found frameworks for this user.",
+        "timestamp": 1718203200000,
+        "data": [
+            {
+                "framework_name": "framework1",
+                "creation_date": "2021-10-05",
+                "environmental_risk_metrics": {
+                    "indicator_er_weight": 0.3,
+                    "metric1": 0.5,
+                    "metric2": 0.3,
+                    "metric3": 0.2
+                },
+                "environmental_opportunity_metrics": {
+                    "indicator_eo_weight": 0.2,
+                    "metric1": 0.5,
+                    "metric2": 0.3,
+                    "metric3": 0.2
+                },
+                "social_risk_metrics": {
+                    "indicator_sr_weight": 0.25,
+                    "metric1": 0.5,
+                    "metric2": 0.3,
+                    "metric3": 0.2
+                },
+                "social_opportunity_metrics": {
+                    "indicator_so_weight": 0.15,
+                    "metric1": 0.5,
+                    "metric2": 0.3,
+                    "metric3": 0.2
+                },
+                "governance_risk_metrics": {
+                    "indicator_gr_weight": 0.05,
+                    "metric1": 0.5,
+                    "metric2": 0.3,
+                    "metric3": 0.2
+                },
+                "governance_opportunity_metrics": {
+                    "indicator_go_weight": 0.05,
+                    "metric1": 0.5,
+                    "metric2": 0.3,
+                    "metric3": 0.2
+                }
+            },
+            {
+                "framework_name": "framework2",
+                "creation_date": "2023-10-07",
+                "environmental_risk_metrics": {
+                    "indicator_er_weight": 0.3,
+                    "metric1": 0.5,
+                    "metric2": 0.3,
+                    "metric3": 0.2
+                },
+                "environmental_opportunity_metrics": {
+                    "indicator_eo_weight": 0.2,
+                    "metric1": 0.5,
+                    "metric2": 0.3,
+                    "metric3": 0.2
+                },
+                "social_risk_metrics": {
+                    "indicator_sr_weight": 0.25,
+                    "metric1": 0.5,
+                    "metric2": 0.3,
+                    "metric3": 0.2
+                },
+                "social_opportunity_metrics": {
+                    "indicator_so_weight": 0.15,
+                    "metric1": 0.5,
+                    "metric2": 0.3,
+                    "metric3": 0.2
+                },
+                "governance_risk_metrics": {
+                    "indicator_gr_weight": 0.05,
+                    "metric1": 0.5,
+                    "metric2": 0.3,
+                    "metric3": 0.2
+                },
+                "governance_opportunity_metrics": {
+                    "indicator_go_weight": 0.05,
+                    "metric1": 0.5,
+                    "metric2": 0.3,
+                    "metric3": 0.2
+                }
+            }
+        ]
+    });
+    
+    const [frameworks, setFrameworks] = useState([]);
+    useEffect(() => {
+        const fetchFrameworks = async () => {
+          try {
+            const user_name = localStorage.getItem('username');
+            console.log("frameworks page GET user_name",user_name)
+            const response = await axios.get(`/api/current_framework?user_name="${user_name}"`);
+            console.log("frameworks page GET response",response)
+            if (response.status === 200) {
+                console.log("frameworks page GET response.data.data",response.data.data)
+                const sortedFrameworks = response.data.data.sort((a, b) => new Date(b.creation_date) - new Date(a.creation_date));
+              setFrameworks(sortedFrameworks);
+            } else {
+              console.error('Failed to fetch frameworks:', response.data.message);
+            }
+          } catch (error) {
+            console.error('Error fetching frameworks:', error);
+          }
+        };
+    
+        fetchFrameworks();
+      }, []);
+
+
+
+
     const handleCreateFrame = () => {
         alert('Create your frame clicked!');
     };
@@ -12,17 +129,9 @@ const FrameSelect = () => {
 
     };
 
-    const frames = [
-        { id: 1, name: 'Frame name', intro: 'intro: this is frame A, which is used by ..............' },
-        { id: 2, name: 'Frame name', intro: 'intro: this is frame A, which is used by ..............' },
-        { id: 3, name: 'Frame name', intro: 'intro: this is frame A, which is used by ..............' },
-        { id: 4, name: 'Frame name', intro: 'intro: this is frame A, which is used by ..............' },
-        { id: 5, name: 'Frame name', intro: 'intro: this is frame A, which is used by ..............' },
-        { id: 6, name: 'Frame name', intro: 'intro: this is frame A, which is used by ..............' },
-        { id: 7, name: 'Frame name', intro: 'intro: this is frame A, which is used by ..............' },
 
-    ];
 
+    console.log("frameworks",frameworks)
     return (
         <div className="container mt-5">
             <div className="row mb-4">
@@ -41,15 +150,16 @@ const FrameSelect = () => {
                         </div>
                     </div>
                 </div>
-                {frames.map(frame => (
-                    <div key={frame.id} className="col-3 mb-4">
+                {frameworks.map((framework, index) => (
+                    <div key={index} className="col-3 mb-4">
                         <div className="card h-100 d-flex flex-column justify-content-between">
                             <div className="card-body">
-                                <h5 className="card-title">{frame.name}</h5>
-                                <p className="card-text">{frame.intro}</p>
+                                <h5 className="card-title">Frame name: {framework.framework_name}</h5>
+                                <p className="card-text">Creation Date: {framework.creation_date
+                                }</p>
                             </div>
                             <div className="card-footer bg-transparent border-0 d-flex justify-content-center">
-                                <button className="btn btn-outline-primary" onClick={() => handleSelect(frame.id)}>
+                                <button className="btn btn-outline-primary" onClick={() => handleSelect(index)}>
                                     <Link to="/CompanySearch" style={{ textDecoration: 'none' }}>Select</Link>
                                 </button>
                             </div>
@@ -59,6 +169,35 @@ const FrameSelect = () => {
             </div>
         </div>
     );
+    // return (
+    //     <div className="container  mt-5">
+    //       <div className="row mb-4">
+    //         <div className="col-md-3 mb-3">
+    //           <div className="card border-primary">
+    //             <div className="card-body text-center">
+    //               <div className="card-title">Create your frame</div>
+    //               <button className="btn btn-outline-primary" onClick={() => window.location.href = '/frameCustom'}>
+    //                 +
+    //               </button>
+    //             </div>
+    //           </div>
+    //         </div>
+    //         {frameworks.map((framework, index) => (
+    //           <div key={index} className="col-md-3 mb-3">
+    //             <div className="card">
+    //               <div className="card-body">
+    //                 <h5 className="card-title">Frame {index + 1}</h5>
+    //                 <p className="card-text">intro: {framework.framework_name}</p>
+    //                 <button className="btn btn-outline-primary">Select</button>
+    //               </div>
+    //             </div>
+    //           </div>
+    //         ))}
+    //       </div>
+    //     </div>
+    //   );
+
+
 };
 
 export default FrameSelect;
