402f8834354039c7cfab0016aa7d6a4b9d6f68d5
SinceDreamTen <jim-tan@foxmail.com>
2024-07-24 19:47:54 +1000

    store token when successful sign up

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,26 +1,30 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import {  Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-import MockAdapter from 'axios-mock-adapter';
+import MockAdapter from "axios-mock-adapter";
 const SignUp = () => {
   const mock = new MockAdapter(axios);
 
-  mock.onPost('/api/auth/login').reply(409, {
-    "status": "error",
-    "message": "Username or email already exists."  
+  mock.onPost("/api/auth/login").reply(409, {
+    status: "error",
+    message: "Username or email already exists.",
   });
-  mock.onPost('/api/auth/register').reply(200, {
-    "status": "success",
-    "message": "User registered successfully!"  
+  mock.onPost("/api/auth/register").reply(200, {
+    code: "200",
+    status: 200,
+    message: "Success",
+    timestamp: 1721675472929,
+    data: null,
+    error: null,
   });
   const [userData, setUserData] = useState({
-    username: '',
-    password: '',
-    email: ''
+    username: "",
+    password: "",
+    email: "",
   });
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -30,20 +34,21 @@ const SignUp = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      console.log("post userData",userData);
-      const response = await axios.post('/api/auth/register', userData);
-      console.log("response",response);
+      console.log("post userData", userData);
+      const response = await axios.post("/api/auth/register", userData);
+      console.log("response", response);
       if (response.status === 200) {
-        localStorage.setItem('username', userData.username);
+        localStorage.setItem("username", userData.username);
         console.log("register success");
-        navigate('/SingleMode');//window.location.href = '/SingleMode'; 
+        localStorage.setItem("token", response.data.data);
+        navigate("/SingleMode");
       }
     } catch (error) {
       if (error.response && error.response.status === 409) {
-        console.log("error",error.response);
-        setError('Username or email already exists.');
+        console.log("error", error.response);
+        setError("Username or email already exists.");
       } else {
-        setError('An unexpected error occurred. Please try again.');
+        setError("An unexpected error occurred. Please try again.");
       }
     }
   };
@@ -53,23 +58,57 @@ const SignUp = () => {
       <h2>Sign Up</h2>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="username" className="form-label">Username</label>
-          <input type="text" className="form-control" id="username" name="username" value={userData.username} onChange={handleChange} required />
+          <label htmlFor="username" className="form-label">
+            Username
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            name="username"
+            value={userData.username}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="mb-3">
-          <label htmlFor="password" className="form-label">Password</label>
-          <input type="password" className="form-control" id="password" name="password" value={userData.password} onChange={handleChange} required />
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            value={userData.password}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="mb-3">
-          <label htmlFor="email" className="form-label">Email</label>
-          <input type="email" className="form-control" id="email" name="email" value={userData.email} onChange={handleChange} required />
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            value={userData.email}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="d-flex justify-content-between">
-          <button type="submit" className="btn btn-primary">Sign up</button>
-          <Link to="/login" className="btn">Already have account?</Link>
+          <button type="submit" className="btn btn-primary">
+            Sign up
+          </button>
+          <Link to="/login" className="btn">
+            Already have account?
+          </Link>
         </div>
       </form>
-      
+
       {error && <div className="alert alert-danger mt-2">{error}</div>}
     </div>
   );
