dfafda1c643626e2e3e1c78f3451b1fd2f3fa4fe
SinceDreamTen <jim-tan@foxmail.com>
2024-07-30 22:13:34 +1000

    modified:   src/components/Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
-    return (
-        <header className="d-flex justify-content-between align-items-center p-1 border-bottom">
-            <Link to="/SingleMode" className="text-dark" style={{ textDecoration: 'none' }}> 
-                <div className="h3 mb-0">EcoM ESG Platform</div>
-            </Link>
-            
-            <div>
-                
-            </div>
+  return (
+    <header className="d-flex justify-content-between align-items-center p-1 border-bottom">
+      <Link
+        to="/dashboard"
+        className="text-dark"
+        style={{ textDecoration: "none" }}
+      >
+        <div className="h3 mb-0">EcoM ESG Platform</div>
+      </Link>
 
+      <div></div>
 
-            <div>
-                
-            </div>
-            <div>
-                <button className="btn btn-secondary">
-                    <Link className="text-white" to="/login" style={{ textDecoration: 'none' }}>Log out</Link>
-                </button>
-            </div>
-        </header>
-    );
+      <div></div>
+      <div>
+        <button className="btn btn-secondary">
+          <Link
+            className="text-white"
+            to="/login"
+            style={{ textDecoration: "none" }}
+          >
+            Log out
+          </Link>
+        </button>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
